fd92fdfa9925119bf6602e441989f28b84d3cbc2
maonaparede <jefferson9312@gmail.com>
2020-04-28 20:26:06 -0300

    request_friend finalizado

---
+++ app/src/main/java/com/example/telas_background/Friend_Request.java
@@ -0,0 +1,14 @@
+package com.example.telas_background;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Friend_Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend__request);
+    }
+}
