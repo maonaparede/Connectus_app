293d3b39c58c0c96cf5ca2cf870b7108c1b168e7
maonaparede <jefferson9312@gmail.com>
2020-05-21 20:36:23 -0300

    item Comentário adicionado

---
+++ app/src/main/java/com/example/telas_background/Comentarios_Posts.java
@@ -0,0 +1,14 @@
+package com.example.telas_background;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Comentarios_Posts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comentarios__posts);
+    }
+}
